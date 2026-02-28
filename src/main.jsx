afa8cf61092afd38f0a56f606adfc060026d6a5c
KhailYT <161350348+KhailYT@users.noreply.github.com>
2026-02-27 17:15:48 -0800

    Initialize React application in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,9 @@
-git init
-git add .
-git commit -m "initial commit"
-git remote add origin [https://github.com/Khailyt/myt-landing.git]
-git push -u origin main
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
